6a75109ee98500e07b0093cf6d76cfa2d2a16594
mandatoryrecess <rossanneclamp@gmail.com>
2021-06-23 20:51:54 +0000

    insert challenge

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,30 @@
+
+INSERT INTO users (name, email, password)
+VALUES
+('Louisa Meyer', 'jacksonrose@hotmail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Dominic Parks', 'victoriablackwell@outlook.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Sue Luna', 'jasonvincent@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Rosalie Garza', 'jacksondavid@gmx.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Etta West', 'charlielevy@yahoo.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Margaret Wong', 'makaylaweiss@icloud.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('Leroy Hart', 'jaycereynolds@inbox.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
+
+INSERT INTO reservations (guest_id, property_id, start_date, end_date)
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
+
+INSERT INTO properties(owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, parking_spaces, number_of_bathrooms, number_of_bedrooms, country, street, city, province, post_code, active)
+VALUES (2, 'Habit Mix', 'description', 'https://images.pexels.com/photos/2080018/pexels-photo-2080018.jpeg?auto=compress&cs=tinysrgb&h=35fffsfds0', 'https://images.pexels.com/photos/2080018/pexels-photo-20800fdsfd18.jpeg', 233.50, 0, 3, 2, 'Canada', '1392 Gaza Junction', 'lakeShire', 'Quebec', 00153, true);
+
+INSERT INTO reservations (start_date, end_date, property_id, guest_id)
+VALUES 
+('2018-09-11', '2018-09-26', 2, 3),
+('2019-01-04', '2019-02-01', 2, 2),
+('2021-10-01', '2021-10-14', 1, 4);
+
+ INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+ VALUES 
+ (3, 2, 1, 3, 'Oh Hello There'), 
+ (2, 2, 1, 4, 'I do not want to go to the lake today'), 
+ (4, 1, 2, 4, 'black markers');
